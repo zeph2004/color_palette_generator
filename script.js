d64c866d177c1cdd0e421385afc770c3d2d10637
zeph2004 <zephbryson04@gmail.com>
2026-05-31 12:47:43 +0300

    complementary, triadic and tetradic color palette generator logic added

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateBaseColor() {
     const hue = Math.floor(Math.random() * 360);
 
     // Saturation: 45 - 90
-    const saturation = Math.floor(Math.random() * 46) + 45;
+    const saturation = Math.floor(Math.random() * 56) + 45;
 
     // Lightness: 50 - 80
     const lightness = Math.floor(Math.random() * 31) + 50;
@@ -66,6 +66,98 @@ function generateMonochromaticPalette(baseColor) {
 
 
 // ====================
+// COMPLEMENTARY PALETTE
+// ====================
+
+function generateComplementaryPalette(baseColor) {
+    const palette = [];
+
+    const availableRange =
+        MAX_LIGHTNESS - baseColor.lightness;
+    
+    const interval =
+        availableRange / 2;
+
+    const complementaryHue = (baseColor.hue + 180) % 360
+
+        for (let i = 0; i < 3; i++) {
+        palette.push({
+            hue: baseColor.hue,
+            saturation: baseColor.saturation,
+            lightness:
+                baseColor.lightness +
+                i * interval
+        });
+    }
+
+        for (let i = 0; i < 2; i++) {
+            palette.push({
+                hue: complementaryHue,
+                saturation: baseColor.saturation,
+                lightness: baseColor.lightness + (i*2) * interval
+            });
+    }
+
+    return palette;
+}
+
+// ====================
+// TRIADIC PALETTE
+// ====================
+function generateTriadicPalette(baseColor) {
+    const palette = [];
+
+    const availableRange =
+        MAX_LIGHTNESS - baseColor.lightness;
+    
+    const interval =
+        availableRange / 2;
+    
+    for (let i = 0; i < 3; i++) {
+        palette.push({
+            hue: (baseColor.hue + (120 * i)) % 360,
+            saturation: baseColor.saturation,
+            lightness: baseColor.lightness
+        });
+    }
+    for (let i = 0; i < 2; i++) {
+        palette.push({
+            hue: baseColor.hue,
+            saturation: baseColor.saturation,
+            lightness: baseColor.lightness + ((i + 1) * interval)
+        });
+    }
+    return palette;
+}
+
+// ====================
+// TETRADIC PALETTE
+// ====================
+
+function generateTetradicPalette(baseColor) {
+    const palette = [];
+
+    const availableRange =
+        MAX_LIGHTNESS - baseColor.lightness;
+
+    for (let i = 0; i < 4; i++) {
+        palette.push({
+            hue: (baseColor.hue + (90 * i)) % 360,
+            saturation: baseColor.saturation,
+            lightness: baseColor.lightness
+        });
+    }
+
+    palette.push({
+        hue: baseColor.hue,
+        saturation: baseColor.saturation,
+        lightness: baseColor.lightness + availableRange
+    });
+
+    return palette;
+}
+
+// ====================
 // TESTING
 // ====================
 
@@ -74,8 +166,26 @@ const baseColor = generateBaseColor();
 palettes.monochromatic =
     generateMonochromaticPalette(baseColor);
 
+palettes.complementary =
+    generateComplementaryPalette(baseColor);
+
+palettes.triadic =
+    generateTriadicPalette(baseColor);
+
+palettes.tetradic = 
+    generateTetradicPalette(baseColor);
+
 console.log("Base Color:");
 console.log(baseColor);
 
 console.log("Monochromatic Palette:");
 console.log(palettes.monochromatic);
+
+console.log("Complementary Palette:");
+console.log(palettes.complementary);
+
+console.log("Triadic Palette:");
+console.log(palettes.triadic);
+
+console.log("Tetradic Palette:");
+console.log(palettes.tetradic);
